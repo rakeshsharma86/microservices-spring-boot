e2a43602c68d0724f8338bb593caa520787cd8b4
Rakesh <rakeshsharma86@hotmail.com>
2018-06-30 16:42:02 +0530

    sample rx java async rest service

--- modules/user-management/src/main/java/com/rks/usermanagement/rest/controller/UserController.java
+++ modules/user-management/src/main/java/com/rks/usermanagement/rest/controller/UserController.java
@@ -5,21 +5,27 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
 
 import com.rks.usermanagement.rest.model.User;
+
+import rx.Observable;
 
 @RestController
 public class UserController {
 	
 	@GetMapping(value = "/users")
-	public List<User> getUsers(){
+	public DeferredResult<List<User>> getUsers(){
 		List<User> users = new ArrayList<>();
 		
 		for(int i = 0; i<10; i++){
 			users.add(new User("id"+i, "name"+i, "role"+i));
 		}
 		
-		return users;
+		Observable<List<User>> o = Observable.from(users).toList();
+	    DeferredResult<List<User>> deffered = new DeferredResult<>(90000L);
+	    o.subscribe(m -> deffered.setResult(m), e -> deffered.setErrorResult(e));
+	    return deffered;
 	}
 
 }
